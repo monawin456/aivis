65ea70c184b163751b3667f50211b0781001b9e8
monawin456 <monawin456@gmail.com>
2020-01-16 16:19:46 +0900

    add DB Manager

--- src/aivis/common/User.java
+++ src/aivis/common/User.java
@@ -1,4 +1,22 @@
 package aivis.common;
 
 public class User {
+    public String uID;
+    public String Password;
+    public String uName;
+    public String uEmail;
+
+    public User() {
+        uID = null;
+        Password = null;
+        uName = null;
+        uEmail = null;
+    }
+
+    public User(String uID, String Password, String uName, String uEmail) {
+        this.uID = uID;
+        this.Password = Password;
+        this.uName = uName;
+        this.uEmail = uEmail;
+    }
 }
